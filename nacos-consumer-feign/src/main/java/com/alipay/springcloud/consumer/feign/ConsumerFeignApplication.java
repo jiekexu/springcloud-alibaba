de98294a47c3fb570ac103888ecdbf89e298cb5b
xujiachen <943522184@qq.com>
2020-02-08 23:08:44 +0800

    add

--- nacos-consumer-feign/src/main/java/com/alipay/springcloud/consumer/feign/ConsumerFeignApplication.java
+++ nacos-consumer-feign/src/main/java/com/alipay/springcloud/consumer/feign/ConsumerFeignApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableDiscoveryClient
-@SpringBootApplication
 /**
  * @author xjc
  * @version $Id: ConsumerFeignApplication.java, v 0.1 2019年09月25日 xjc 15:32 Exp $
  */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ConsumerFeignApplication {
 
 	public static void main(String[] args) {
